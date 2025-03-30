fb7bd86f3df17dfea0a4960722b7b25f0f2e1677
JulyMartynova <j.martynova@innopolis.university>
2025-03-30 15:22:41 +0300

    Updated App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ProductsPage from './pages/ProductsPage'
+import ProductsPage from './pages/ProductsPage';
 import CreateProductPage from './pages/CreateProductPage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/my-app">  {/* Укажите вашу подпапку */}
       <Routes>
-      <Route path = "/products" element={<ProductsPage/>}/>
-      <Route path ="/create-product" element = {<CreateProductPage></CreateProductPage>} />
-      <Route path ="/products/:id" element = {<ProductDetailsPage/>} />
-      <Route path="/" element = {<ProductsPage/>} />
+        <Route path="/products" element={<ProductsPage/>}/>
+        <Route path="/create-product" element={<CreateProductPage/>} />
+        <Route path="/products/:id" element={<ProductDetailsPage/>} />
+        <Route path="/" element={<ProductsPage/>} />
       </Routes>
     </Router>
-      
   );
 }
 
